a467870a3ef216643b343d71427d27ba053787a2
jscodin <jascode9@gmail.com>
2023-11-29 21:19:59 +0000

    Create fns for all of the basic math operators

--- topcalculator.js
+++ topcalculator.js
@@ -1,8 +1,32 @@
 // the basic calculations
 // add
+function add(a, b) {
+  return a + b
+}
+
+console.log(add(1, 2));
 
 // subtract
 
+function sub(a, b) {
+  return a - b
+}
+
+console.log(sub(100, 2));
+
 // divide
+function divide(a, b) {
+  return a / b
+}
+
+console.log(divide(150, 5));
 
 // multiply
+function mult(a, b) {
+  return a * b
+}
+
+console.log(mult(40, 30));
+
+
+
