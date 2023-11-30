bf86afe24455270809406c7d7f66ffaf3c6da1bb
jscodin <jascode9@gmail.com>
2023-11-30 00:11:08 +0000

    clean up comments & formatting

--- topcalculator.js
+++ topcalculator.js
@@ -1,32 +1,37 @@
 // the basic calculations
-// add
+// +
 function add(a, b) {
   return a + b
 }
-
 console.log(add(1, 2));
 
-// subtract
-
+// -
 function sub(a, b) {
   return a - b
 }
-
 console.log(sub(100, 2));
 
-// divide
+// /
 function divide(a, b) {
   return a / b
 }
-
 console.log(divide(150, 5));
 
-// multiply
+// *
 function mult(a, b) {
   return a * b
 }
-
 console.log(mult(40, 30));
+
+// operations
+
+// let num1;
+// let op;
+// let num2;
+//
+// function operate(numA, operator, numB) {
+//   add(numA, numB);
+// }
 
 
 
