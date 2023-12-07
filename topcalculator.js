4bbe7835c270b27516c7a486344aafb179e1a4c6
jscodin <jascode9@gmail.com>
2023-12-07 22:44:45 +0000

    Add functionality to store numbers and operators
    - refactor functions to allow storage of the first set of numbers
      before the operator is pressed
    - add functionality to store the second set of numbers after the
      operator is pressed

--- topcalculator.js
+++ topcalculator.js
@@ -29,27 +29,77 @@ console.log(mult(40, 30));
 // let op;
 // let num2;
 //
-// storing the digit buttons
-const digitBtns = document.querySelectorAll(".digit");
-
+// Global variables
+const btn = document.querySelectorAll("button");
+const numbers = document.querySelectorAll(".digit");
 const disp = document.querySelector(".display");
+const operator = document.querySelectorAll(".operator");
 
+
+let num1 = "";
+let num2 = "";
+let opr = "";
 let numsOnDisp;
 
 // displays digit buttons on display
-digitBtns.forEach((button) => {
-  button.addEventListener('click', () => {
+// numbers.forEach((number) => {
+//   number.addEventListener('click', () => {
+//     if (disp.textContent.length < 8) {
+//       disp.textContent += number.id
+//     };
+//     if (opr === "") {
+//       num1 += disp.textContent;
+//       console.log(num1);
+//     };
+//   });
+// });
+numbers.forEach((number) => {
+  number.addEventListener('click', () => {
     if (disp.textContent.length < 8) {
-      disp.textContent += button.id
+      disp.textContent += number.id
+      if (opr === "") {
+        num1 += number.id;
+        console.log(num1);
+      } else {
+        disp.textContent = "";
+        disp.textContent += number.id;
+        num2 += number.id;
+        console.log(num2);
+      }
     };
-    numsOnDisp = disp.textContent;
-    console.log(numsOnDisp = disp.textContent);
   });
 });
-console.log(numsOnDisp);
+
+operator.forEach((button) => {
+  button.addEventListener('click', (e) => {
+    opr = e.target.innerText;
+    console.log(opr);
+    // opr = button.id;
+    // console.log(numsOnDisp += button.innerText);
+    // numsOnDisp += button.innerText;
+    // disp.textContent += button;
+    //
+    // console.log(opr = button.id);
+  });
+});
+
+// store operator buttons
+// operator.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     opr = button.id;
+//
+//     console.log(opr = button.id);
+//   });
+// });
+
+
+// when user presses the = sign
+
 
 function operate(numA, operator, numB) {
+
   add(numA, numB);
+
 }
 
 
@@ -59,10 +109,9 @@ const clear = document.querySelector("#clear");
 clear.addEventListener('click', () => {
   disp.textContent = "";
 });
-
+// delete last number input
 const del = document.querySelector("#delete");
 del.addEventListener('click', () => {
   disp.textContent = disp.textContent.slice(0, -1);
   numsOnDisp = disp.textContent;
-  console.log(numsOnDisp = disp.textContent);
 });
