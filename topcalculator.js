9d720fda159a899fe8942ed49906d6992816b004
jscodin <jascode9@gmail.com>
2023-12-08 22:44:25 +0000

    clean up comments & formatting

--- topcalculator.js
+++ topcalculator.js
@@ -42,17 +42,6 @@ let opr = "";
 let numsOnDisp;
 
 // displays digit buttons on display
-// numbers.forEach((number) => {
-//   number.addEventListener('click', () => {
-//     if (disp.textContent.length < 8) {
-//       disp.textContent += number.id
-//     };
-//     if (opr === "") {
-//       num1 += disp.textContent;
-//       console.log(num1);
-//     };
-//   });
-// });
 numbers.forEach((number) => {
   number.addEventListener('click', () => {
     if (disp.textContent.length < 8) {
@@ -62,9 +51,11 @@ numbers.forEach((number) => {
         console.log(num1);
       } else {
         disp.textContent = "";
-        disp.textContent += number.id;
-        num2 += number.id;
-        console.log(num2);
+        if (disp.textContent.length < 8) {
+          disp.textContent += number.id;
+          num2 += number.id;
+          console.log(num2);
+        }
       }
     };
   });
@@ -74,28 +65,10 @@ operator.forEach((button) => {
   button.addEventListener('click', (e) => {
     opr = e.target.innerText;
     console.log(opr);
-    // opr = button.id;
-    // console.log(numsOnDisp += button.innerText);
-    // numsOnDisp += button.innerText;
-    // disp.textContent += button;
-    //
-    // console.log(opr = button.id);
   });
 });
 
-// store operator buttons
-// operator.forEach((button) => {
-//   button.addEventListener('click', () => {
-//     opr = button.id;
-//
-//     console.log(opr = button.id);
-//   });
-// });
-
-
 // when user presses the = sign
-
-
 function operate(numA, operator, numB) {
 
   add(numA, numB);
